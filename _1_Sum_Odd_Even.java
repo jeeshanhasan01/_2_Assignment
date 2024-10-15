aa700878eca3a65fa4b448d275d1c08ec8dfc68f
Jeeshan Hasan <jeeshanhasan786@gmail.com>
2024-10-16 00:05:47 +0530

    Create _1_Sum_Odd_Even.java
    Sum of odd placed and even placed digits

---
+++ _1_Sum_Odd_Even.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // Take input n
+		int index = 1; // it's indexing of n
+		int evenSum = 0; // store the sum of evenPlaces
+		int oddSum = 0; // store the sum of oddPlaces
+		// loop trough all n
+		while (n > 0) {
+
+			int rem = n % 10; // extract the last digit of n
+			n = n / 10; // remove the last digit from n
+
+			// check if the current index digit is even or odd
+			if (index % 2 == 0) {
+				evenSum = evenSum + rem;
+			} else {
+				oddSum = oddSum + rem;
+			}
+			index++;
+		}
+		System.out.println(evenSum);
+		System.out.println(oddSum);
+	}
+}
