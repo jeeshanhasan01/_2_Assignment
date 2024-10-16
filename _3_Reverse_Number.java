7b13cf7f4099d76d59c00662633aaeaf5424be42
Jeeshan Hasan <jeeshanhasan786@gmail.com>
2024-10-16 12:11:26 +0530

    Update _3_Reverse_Number.java

--- _3_Reverse_Number.java
+++ _3_Reverse_Number.java
@@ -2,7 +2,7 @@ package Assignment;
 
 import java.util.*;
 
-public class Reverse_Number {
+public class _3_Reverse_Number {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
